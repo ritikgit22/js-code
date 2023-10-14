da4fc5973ae27154af68659e0bede88826279542
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-14 09:43:29 +0000

    dt done

---
+++ 01basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict" ; // treeat all JS code as newer version
+
+// alert(3+4) : this will not get execute because we are using nodejs not browser
+
+let namee = "ritik"
+let age = 18
+let isloggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null => stand-alone value(null is a object datatype)
+// undefined (this is of undefined datatype)
+// symbol => unique
+// object
+
+console.table([namee , age , isloggedIn])
