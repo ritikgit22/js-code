b04a318c600580bdf445546e5398ad243272ab58
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-14 08:19:42 +0000

    vari donee

---
+++ 01basics/variabless.js
@@ -0,0 +1,11 @@
+const accountId = 123321
+let accountEmail = "ritik@gmail.com"
+var accontPassword = "456787"
+accountcity = "Jaipur"
+let accountstate;
+
+console.log(accountEmail);
+
+// preferred not to use var because of issue in block scope and functional scope //
+
+console.table([accountId , accontPassword , accountcity , accountstate])
