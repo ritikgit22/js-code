1ff8b38e23af35453d53a6a63cc1cc9cce9925b2
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-15 11:21:47 +0000

    oper done

---
+++ 01basics/operations.js
@@ -0,0 +1,33 @@
+// Operations //
+
+let value = 2
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " ritik"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+3);
+console.log(1+2+"3");
+console.log(1+"2"+3);
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+//link : 
+//link : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
