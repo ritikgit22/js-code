5e92c3ef32bd51a869f5bed64de8db6562a43bbe
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-15 15:36:04 +0000

    dtsummary done

---
+++ 01basics/dtsummary.js
@@ -0,0 +1,31 @@
+// Primitive 
+// String , Number , Boolean , Null , undefined , Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.5
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherID = Symbol('123')
+
+console.log(id === anotherID);
+// Symbol is used to make the same type of values unique.
+const bigNumber = 123456787654321n
+
+// Non Premitive (Reference)
+// Array , Objects , Functions
+
+const cric = ["MSD" , "Virat Kohli" , "Rohit Sharma"];
+let myobj = {
+    name : "Ritik" ,
+    age : 22 ,
+}
+
+const myfunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof anotherID)
+
+// link : https://262.ecma-international.org/5.1/#sec-11.4.3
