8c875db7c14bb512011a041fbbbaef70dc1b9a2a
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-17 16:36:47 +0000

    string done

---
+++ 01basics/strings.js
@@ -0,0 +1,14 @@
+const name = "ritik"
+const repoCount = 22
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count value is ${repoCount}`);
+
+const gameName = new String('ritikBH')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt);
+console.log(gameName.indexOf);
