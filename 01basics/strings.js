81bd84232f3a29ffb4236c79156273c56e9793ea
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-18 17:40:20 +0000

    string donee

--- 01basics/strings.js
+++ 01basics/strings.js
@@ -4,11 +4,26 @@ const repoCount = 22
 
 console.log(`Hello my name is ${name} and my repo count value is ${repoCount}`);
 
-const gameName = new String('ritikBH')
+const gameName = new String('ritik=BH')
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 
 console.log(gameName.length);
-console.log(gameName.toUpperCase);
-console.log(gameName.charAt);
-console.log(gameName.indexOf);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0 , 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-5 , 2)
+console.log(anotherString);
+
+const newString1 = "    hitesh    "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://ritikkumar.com/ritik%20kumar"
+console.log(url.replace('%20' , '-'))
+console.log(url.includes('ombi'))
+console.log(gameName.split('='))
