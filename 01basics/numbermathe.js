938fab3dc1795a98c503349e9f48cbea938c70f5
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-19 16:30:57 +0000

    nums and mahte done

--- 01basics/numbermathe.js
+++ 01basics/numbermathe.js
@@ -16,10 +16,19 @@ console.log(hundreds.toLocaleString('en-IN'));
 //  ++++++++++++++++++++ Maths +++++++++++++++++++++
 
 console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.9));
-console.log(Math.min( 2 , 5 , 8 , 3 ));
-console.log(Math.max( 9 , 7 , 4 , 5 ));
+console.log(Math.abs(-4));  // is used to get the positive of a number.
+console.log(Math.round(4.6)); // is used to round a number.
+console.log(Math.ceil(4.2)); // is used to get an upper round number.
+console.log(Math.floor(4.9)); // is used to get a lower round number.
+console.log(Math.min( 2 , 5 , 8 , 3 )); // for minimum number.
+console.log(Math.max( 9 , 7 , 4 , 5 )); // for maximum number.
 
+console.log(Math.random());
+console.log((Math.random()*10 + 1));
+
+// The following method is used when we need a random number between a desired range.
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))) + 1
