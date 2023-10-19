a1e75cfb85e4392784188aaaeda53a5c6ec5657e
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-19 15:46:28 +0000

    nums done

---
+++ 01basics/numbermathe.js
@@ -0,0 +1,25 @@
+const score = 500
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.4567
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//  ++++++++++++++++++++ Maths +++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min( 2 , 5 , 8 , 3 ));
+console.log(Math.max( 9 , 7 , 4 , 5 ));
+
