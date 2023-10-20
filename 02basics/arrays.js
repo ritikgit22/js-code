7e347d5c9735641e449879e701c659b22e92d55b
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-20 15:15:11 +0000

    arrays1 done

---
+++ 02basics/arrays.js
@@ -0,0 +1,13 @@
+// arrays
+
+const myArr = [0 , 1 , 2 , 3 , 4 , 5]
+const cric = ["MSD" , "Virat" , "Shikhar" , "Rohit"]
+
+const myArr2 = new Array(1 , 2 ,3 , 4 , 5)
+console.log(myArr[2]);
+
+// Array Methods
+
+myArr.push(6)
+myArr.pop()
+console.log(myArr);
