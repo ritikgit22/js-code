f8537489f7b384dba006322766b51ccdaa96d56e
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-28 15:43:57 +0000

    function done

--- 03basics/functions1.js
+++ 03basics/functions1.js
@@ -29,3 +29,32 @@ function loginmess(username="sam"){
     return `${username} just logged in`
 }
 console.log(loginmess("Ritik")) // return is not same as console; to print something we have to use console keyword
+
+
+// function calcartprice(val1, val2, ...num1){
+//     return num1
+// }
+// console.log(calcartprice(100,200,300,400,500))
+
+// const user = {
+//     username : "ritik",
+//     price : 199
+// }
+
+// function handleobj(anyobj){
+//     console.log(`username is ${anyobj.username} and the price is ${anyobj.price}`);
+// }
+// handleobj(anyobj)
+
+handleobj({
+    username : "sam"
+    price : 299
+})
+
+const mynewarray = [100,200,300,400]
+function returnsecvalue(getarray){
+    return getarray[1]
+}
+
+//console.log(returnsecvalue(mynewarray));
+console.log(returnsecvalue([100,200,300,400]));
