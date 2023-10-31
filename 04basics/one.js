7f0d466955f21899fab494e90613526375b0ca6f
ritikgit22 <146740911+ritikgit22@users.noreply.github.com>
2023-10-31 15:18:38 +0000

    doneee

---
+++ 04basics/one.js
@@ -0,0 +1,52 @@
+// if
+const isUserloggedin = true
+const temperature = 41
+
+if (temperature === 40){
+    console.log("less than 50");
+} else {
+    console.log("temperature is greater than 50");
+}
+
+consoele.log("Executed.")
+// < , > , <= , >= , == , != , === , !==
+
+const score = 200
+
+if (score > 100){
+    const power = "fly"
+    console.log(`user power : ${power}`);
+}
+
+//console.log(`user power : ${power}`);  ^it will not get executed because it is out of scope.
+// const and let is user to execute the condition within the same scope and var is user to execute the condition out 0of the scope.
+
+
+const balance = 1000
+
+//if (balance > 500) console.log("test") , console.log("test2");   ^not a good way to write a code due to its complex readability.
+
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+} else if (balance < 900) {
+    console.log("less than 900");
+} else {
+    console.log("less than 1200");
+}
+
+const Userloggedin = true
+const debitCard = true
+const loggedinfromGoogle = false
+const loggedinfromEmail = true
+
+if (Userloggedin && debitCard && 2 == 2) {
+    console.log("Allow to buy course.");
+}
+
+if (loggedinfromGoogle || loggedinfromEmail) {
+    console.log("User Logged In");
+}
+
+// && and || are called the logical operators. 
---
+++ 04basics/switch.js
@@ -0,0 +1,51 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1 :
+//         console.log("January");
+//         break;
+//     case 2 :
+//         console.log("February");
+//         break;
+//     case 3 :
+//         console.log("March");
+//         break;  
+//     case 4 :
+//         console.log("April");
+//         break;      
+
+//     default:
+//         console.log("default case matched.");
+//         break;
+// }
+
+// break is used to stop the code right there and dont let the further program to be executed.
+
+const month = "March"
+switch (month) {
+    case "January" :
+        console.log("This is January.");
+        break;
+    case "February" :
+        console.log("This is February.");
+        break;
+    case "March" :
+        console.log("This is March.");
+        break;  
+    case "April" :
+        console.log("This is April.");
+        break;      
+
+    default:
+        console.log("default case matched.");
+        break;
+}
+// to match the string we write that string right after the case function.
